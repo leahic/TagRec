f298a26eb73c003638863d83c9163afdae3a953a
Leahic <leahic@163.com>
2017-04-05 15:53:43 +0800

    change for BibSonomy

--- data_split.py
+++ data_split.py
@@ -9,9 +9,6 @@ def str_getuser(s):
 def str_gettime(s):
     return int(s.split(';')[2].replace('"',''))
 
-def str_getitem(s):
-    return int(s.split(';')[1].replace('"',''))
-
 if __name__ == '__main__':
     print sys.argv[1]
     print sys.argv[2]
@@ -37,12 +34,13 @@ if __name__ == '__main__':
     for user_index , user in enumerate(Userlist):
         raw = Records[user]
         raw = sorted(raw , key = lambda x : str_gettime(x))
-        lasttime = str_gettime(raw[-1])
-        for line in raw:
-            if str_gettime(line) < lasttime:
-                f2.write(line)
-            else:
-                f3.write(line)
+        itemnums = len(raw)
+        testnums = min(1 , itemnums - 1)
+        trainnums = itemnums - testnums
+        for line in raw[0:trainnums]:
+            f2.write(line)
+        for line in raw[trainnums:]:
+            f3.write(line)
 
     f2.close()
     f3.close()
